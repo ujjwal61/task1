1028da2637303bfde99c75a28e287c1294f10e99
ujjwal61 <66882758+ujjwal61@users.noreply.github.com>
2020-06-16 12:10:54 +0530

    commit

---
+++ task.php
@@ -0,0 +1,11 @@
+
+  # Task1
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>WELCOME TO MY TASK1</title>
+</head>
+<body>
+    <img src='path/to/terraimg.jpg' alt='photo of me' />
+</body>
+</html>
